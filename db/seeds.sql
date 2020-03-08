40cc587f0637135b3326038ea90a7819432436b7
Caitlin Trask <caitlintrask@gmail.com>
2020-03-08 16:12:59 -0600

    worked with tutor to add functionality and database with handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Bleu Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Avo Yum');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-status").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  
+      var newState = {
+        devoured: newDevoured
+      };
+  
+      // Send the PUT request
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();  
+          // $(".change-status").hide()
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val()
+      };
+  
+      // Send the POST request
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
